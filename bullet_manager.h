e9cab112f1e2a73c00701fec3f02859586285660
EmulatedSeasons <89668582+EmulatedSeasons@users.noreply.github.com>
2022-03-04 17:09:14 -0500

    Fixed bullet_manager.cpp

--- bullet_manager.h
+++ bullet_manager.h
@@ -3,6 +3,8 @@
 #ifndef BULLET_MANAGER_H
 #define BULLET_MANAGER_H
 
+// Unsure why class_db.h is needed for object.h to work
+#include "core/object/class_db.h"
 #include "core/object/object.h"
 
 class BulletManager : public Object {
